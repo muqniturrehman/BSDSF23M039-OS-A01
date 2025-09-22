a25d9afa175ac49404aa6b519a157374adb34fbc
root <root@DESKTOP-BE3R72P.localdomain>
2025-09-22 20:39:00 +0500

    Implement multifile-build with Makefile

--- include/myfilefunctions.h
+++ include/myfilefunctions.h
@@ -1,7 +1,9 @@
+// Count the number of lines, words and characters in the passed file 
+//stream pointer. Return 0 on success and -1 on failure. 
 
-// Count the number of lines, words and chracters in the passed file stream pointer. Return 0 on success and -1 on failure.
-int wordcount(FILE* file, int* lines, int* words, int* chars);
+int wordCount(FILE* file, int* lines, int* words, int* chars);
+ 
+// Search lines containing search_str in a file, and fills the matches 
+//array. Return the count of matches and  -1 on failure. 
 
-// Search lines containing search_str in a file, and fills the matches array. Return the count of matches and -1 on filure. 
-int mygrep(FILES* fp, const char* search_str, char** matches);
-
+int mygrep(FILE* fp, const char* search_str, char*** matches); 
